6beea0b4c329d589325a51aa22f8faa1b655598b
mathi <Mathiasmundtkofod@gmail.com>
2026-01-04 14:48:57 +0100

    Created some of the base structure -Created welcome page (not populated) -Created game page (not populated) -Created a serial component, to easier test serial communication

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,20 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
+import {WelcomePage} from './pages/welcome-page/welcome-page';
+import {GamePage} from './pages/game-page/game-page';
+import {SerialPlayground} from './pages/serial-playground/serial-playground';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'start',
+    component: WelcomePage
+  },
+  {
+    path: 'game',
+    component: GamePage
+  },
+  {
+    path: 'serial',
+    component: SerialPlayground
+  },
+  {path: '**', redirectTo: 'start'}
+];
