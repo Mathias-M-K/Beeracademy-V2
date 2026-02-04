600836964b9f6b2f2199ad62e6c96927299d3259
mathi <Mathiasmundtkofod@gmail.com>
2026-02-04 14:42:13 +0100

    Fix typo: Rename `timerReports` to `timeReports` in `GameEndEventDto` definition

--- backend/src/main/java/dk/mathiaskofod/services/session/events/game/GameEndEventDto.java
+++ backend/src/main/java/dk/mathiaskofod/services/session/events/game/GameEndEventDto.java
@@ -9,7 +9,7 @@ import dk.mathiaskofod.services.session.models.annotations.EventType;
 import java.util.List;
 
 @EventType("GAME_END")
-public record GameEndEventDto(GameReport gameReport, List<PlayerReport> playerReports, TimerReports timerReports) implements GameEventDto {
+public record GameEndEventDto(GameReport gameReport, List<PlayerReport> playerReports, TimerReports timeReports) implements GameEventDto {
     public static GameEndEventDto fromGameEvent(EndGameEvent event){
         return new GameEndEventDto(event.gameReport(), event.playerReports(), event.timerReports());
     }
